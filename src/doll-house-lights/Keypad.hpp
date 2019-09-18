0de22fe6cdca3c7236d94e4bac3dff5891f4fb3f
Zaar Hai <haizaar@haizaar.com>
2019-09-18 20:08:09 +1000

    Added key definitions.

---
+++ src/doll-house-lights/Keypad.hpp
@@ -0,0 +1,149 @@
+#pragma once
+
+namespace keypad {
+
+enum Keys {
+  Power = 0xFF02FD,
+
+  CopyColor  = 0xFFE817,
+  PasteColor = 0xFFC837,
+
+  BrightnessUp = 0xFF3AC5,
+  BrightnessDown = 0XFFBA45,
+
+  // Colors
+  // Numbers mean x/y button index from bottom left side
+  // of the keypad color block
+  Color11 = 0xFF18E7,
+  Color12 = 0xFF9867,
+  Color13 = 0xFF58A7,
+  Color14 = 0xFFD827,
+
+  Color21 = 0xFF38C7,
+  Color22 = 0xFFB847,
+  Color23 = 0xFF7887,
+  Color24 = 0xFFF807,
+
+  Color31 = 0xFF0AF5,
+  Color32 = 0xFF8A75,
+  Color33 = 0xFFB24D,
+  Color34 = 0xFF32CD,
+
+  Color41 = 0xFF2AD5,
+  Color42 = 0xFFAA55,
+  Color43 = 0xFF926D,
+  Color44 = 0xFF12ED,
+
+  Color51 = 0xFF1AE5,
+  Color52 = 0xFF9A65,
+  Color53 = 0xFFA25D,
+  Color54 = 0xFF22DD,
+
+  // HSV
+  HSVHueUp   = 0xFF28D7,
+  HSVHueDown = 0xFF08F7,
+  HSVSatUp   = 0xFFA857,
+  HSVSatDown = 0xFF8877,
+  HSVValUp   = 0xFF6897,
+  HSVValDown = 0xFF48B7,
+
+  // Room selection
+  RoomBL = 0xFF20DF,
+  RoomBR = 0xFFA05F,
+  RoomML = 0xFF10EF,
+  RoomMR = 0xFF906F,
+  RoomTL = 0xFF30CF,
+  RoomTR = 0xFFB04F,
+  RoomEave = 0xFF708F,
+  RoomAll = 0xFF609F,
+};
+
+bool isCopyPaste(unsigned long keycode) {
+  switch(keycode) {
+    case CopyColor:
+    case PasteColor:
+      return true;
+      break;
+    default:
+      return false;
+      break;
+  }
+}
+
+bool isBrightness(unsigned long keycode) {
+  switch(keycode) {
+    case BrightnessUp:
+    case BrightnessDown:
+      return true;
+      break;
+    default:
+      return false;
+      break;
+  }
+}
+
+bool isColor(unsigned long keycode) {
+  switch(keycode) {
+    case Color11:
+    case Color12:
+    case Color13:
+    case Color14:
+    case Color21:
+    case Color22:
+    case Color23:
+    case Color24:
+    case Color31:
+    case Color32:
+    case Color33:
+    case Color34:
+    case Color41:
+    case Color42:
+    case Color43:
+    case Color44:
+    case Color51:
+    case Color52:
+    case Color53:
+    case Color54:
+      return true;
+      break;
+    default:
+      return false;
+      break;
+  }
+}
+
+bool isHSV(unsigned long keycode) {
+  switch(keycode) {
+    case HSVHueUp:
+    case HSVHueDown:
+    case HSVSatUp:
+    case HSVSatDown:
+    case HSVValUp:
+    case HSVValDown:
+      return true;
+      break;
+    default:
+      return false;
+      break;
+  }
+}
+
+bool isRoom(unsigned long keycode) {
+  switch(keycode) {
+    case RoomBL:
+    case RoomBR:
+    case RoomML:
+    case RoomMR:
+    case RoomTL:
+    case RoomTR:
+    case RoomEave:
+    case RoomAll:
+      return true;
+      break;
+    default:
+      return false;
+      break;
+  }
+}
+
+}  // end namespace
--- src/doll-house-lights/doll-house-lights.ino
+++ src/doll-house-lights/doll-house-lights.ino
@@ -2,7 +2,7 @@
 #include <Adafruit_DotStar.h>
 
 #include "SmartIR.hpp"
-
+#include "Keypad.hpp"
 
 // ##### Config #####
 const byte IR_PIN = 4;
@@ -10,14 +10,38 @@ const byte IR_PIN = 4;
 // ##### Main #####
 SmartIR ir(IR_PIN);
 
+
 void setup() {
   Serial.begin(115200);
   ir.start();
 }
 
+
 void loop() {
-  unsigned long ir_code = 0;
-  if (ir_code=ir.recv()) {
-    Serial.println(ir_code, HEX);
+  unsigned long keycode = 0;
+  if (keycode=ir.recv()) {
+    Serial.print("Received key: ");
+    Serial.println(keycode, HEX);
+
+    if (keypad::isBrightness(keycode)) {
+      Serial.println("It's a brightness key");
+    } else if (keypad::isColor(keycode)) {
+      Serial.println("It's a color key");
+    } else if (keypad::isHSV(keycode)) {
+      Serial.println("It's a HSV key");
+    } else if (keypad::isRoom(keycode)) {
+      Serial.println("It's a Room selection key");
+    } else if (keypad::isCopyPaste(keycode)) {
+      Serial.println("It's a copy/paste key");
+    } else {
+      switch(keycode) {
+        case keypad::Power:
+          Serial.println("It's a power key");
+          break;
+        default:
+          Serial.println("Non relevant key");
+          break;
+      }
+    }
   }
 }
